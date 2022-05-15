2754ce799caeba9955a639396ae734c00dbb588b
kilewa <kilewageorge230@gmail.com>
2022-05-15 13:32:40 +0300

    Created navbar

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import styled from "styled-components"
+import Header from './Header';
+
+
+const Home = () => {
+  return (
+    <>
+      <Header/>
+      <div>Home</div>
+    </>
+    
+
+  )
+}
+
+export default Home;
